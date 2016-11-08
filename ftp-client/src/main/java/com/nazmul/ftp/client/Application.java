ad81552932491c00ed34ab0f9cff0651ba3f0a40
codingbat <Nazmul.Alam@students.ittralee.ie>
2016-11-08 22:21:21 +0000

    started gui for client

--- ftp-client/src/main/java/com/nazmul/ftp/client/Application.java
+++ ftp-client/src/main/java/com/nazmul/ftp/client/Application.java
@@ -1,10 +1,19 @@
 package com.nazmul.ftp.client;
 
+import javax.swing.*;
+
 /**
  * FTP Client Application
  */
 public class Application {
     public static void main(String... args) {
         System.out.println("FTP Client");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+
+            }
+        });
     }
 }
---
+++ ftp-client/src/main/java/com/nazmul/ftp/client/GUI.java
@@ -0,0 +1,24 @@
+package com.nazmul.ftp.client;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame implements ActionListener {
+
+    private JPanel backPanel;
+    private JToolBar toolBar;
+
+    public GUI() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
