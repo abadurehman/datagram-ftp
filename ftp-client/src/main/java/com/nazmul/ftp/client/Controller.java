f2bba5afe656a0c9d0e50d279b724375b0934230
codingbat <Nazmul.Alam@students.ittralee.ie>
2016-11-09 02:22:29 +0000

    reading system files list

--- ftp-client/src/main/java/com/nazmul/ftp/client/Controller.java
+++ ftp-client/src/main/java/com/nazmul/ftp/client/Controller.java
@@ -1,10 +1,21 @@
 package com.nazmul.ftp.client;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
-public class Controller {
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
 
     @FXML
     private TextArea logTextarea;
@@ -13,7 +24,36 @@ public class Controller {
     private Button connectButton;
 
     @FXML
+    private TreeView<File> systemTreeview;
+
+    @FXML
+    private TextArea eventTextarea;
+
+
+    @FXML
     public void test() {
-        logTextarea.appendText("You have successfully connected to the system!");
+        logTextarea.appendText(LocalDate.now() + " " + LocalTime.now() + ": You have successfully connected to the system!\n");
+    }
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+        File currentDir = new File("/home/nazmul/Desktop/datagram-ftp"); // current directory
+        findFiles(currentDir);
+    }
+
+    public void findFiles(File dir) {
+        TreeItem<File> root = new TreeItem<File>(new File("Files:"));
+        root.setExpanded(true);
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                findFiles(file);
+            } else {
+                root.getChildren().add(new TreeItem<File>(file));
+            }
+            root.getChildren().add(new TreeItem<File>(file));
+        }
+
+        systemTreeview.setRoot(root);
     }
 }
--- ftp-client/src/main/java/com/nazmul/ftp/client/GUI.java
+++ ftp-client/src/main/java/com/nazmul/ftp/client/GUI.java
@@ -2,10 +2,8 @@ package com.nazmul.ftp.client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -13,6 +11,11 @@ import javafx.stage.Stage;
  * FTP Client Application
  */
 public class GUI extends Application {
+
+    public static void main(String... args) {
+        System.out.println("FTP Client");
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -24,10 +27,5 @@ public class GUI extends Application {
         primaryStage.setResizable(false);
 
         primaryStage.show();
-    }
-
-    public static void main(String... args) {
-        System.out.println("FTP Client");
-        launch(args);
     }
 }
