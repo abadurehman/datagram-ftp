6bd80f0a4ee9877be7bbb486ac66a37a4b79ccd0
codingbat <Nazmul.Alam@students.ittralee.ie>
2016-11-29 23:48:04 +0000

    START::command pattern created login command pattern to remove switch case smell in the server module state pattern is another choice still have to try out the code in action

---
+++ ftp-server/src/main/java/com/nazmul/ftp/server/Command.java
@@ -0,0 +1,21 @@
+package com.nazmul.ftp.server;
+
+import com.nazmul.ftp.common.Data;
+import com.nazmul.ftp.common.DataSocket;
+import com.nazmul.ftp.server.auth.User;
+
+import java.io.IOException;
+
+/**
+ * Intent
+ *  Encapsulate a request as an object, thereby letting you parametrize clients with different
+ * requests, queue or log requests, and support undoable operations.
+ *  Promote "invocation of a method on an object" to full object status
+ *  An object-oriented callback
+ */
+public interface Command {
+  User execute(short opcode,
+               String message,
+               Data request,
+               DataSocket socket) throws IOException;
+}
---
+++ ftp-server/src/main/java/com/nazmul/ftp/server/LoginCommand.java
@@ -0,0 +1,29 @@
+package com.nazmul.ftp.server;
+
+import com.nazmul.ftp.common.Data;
+import com.nazmul.ftp.common.DataSocket;
+import com.nazmul.ftp.common.protocol.ProtocolCode;
+import com.nazmul.ftp.server.auth.User;
+import com.nazmul.ftp.server.session.LoginPacket;
+
+import java.io.IOException;
+
+/**
+ * Switch Statement Smell -- converting switch cases to Command pattern
+ *
+ * Switch-statements are not an antipattern per se, but if you're coding object oriented you should
+ * consider if the use of a switch is better solved with polymorphism
+ * instead of using a switch statement.
+ */
+public class LoginCommand implements Command {
+
+  @Override
+  public User execute(short opcode,
+                      String message,
+                      Data request,
+                      DataSocket socket) throws IOException {
+
+    LoginPacket loginPacket = new LoginPacket();
+    return loginPacket.processAuthentication(ProtocolCode.LOGIN, message, request, socket);
+  }
+}
